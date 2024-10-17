a06b5454e56e6d25d457b4b833c31929949dde24
Shanshan Fu <jesse.fu21@gmail.com>
2024-10-17 13:10:12 +0100

    testing map

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,11 @@
 class SitesController < ApplicationController
   def index
     @sites = Site.all
+    @markers = @sites.geocoded.map do |site|
+      {
+        lat: site.latitude,
+        lng: site.longitude
+      }
+    end
   end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,9 @@
 class Site < ApplicationRecord
   belongs_to :user
 
-  has_many :schedules
+  has_many :schedules, dependent: :destroy
   validates :name, :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
