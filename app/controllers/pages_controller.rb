b2e8369cd0eaa71173e8726051fb45c45e1a379d
Shanshan Fu <jesse.fu21@gmail.com>
2024-10-21 16:50:10 +0100

    add dashboard to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @home_page = true
+  end
+
+  def dashboard
+    @sites = Site.all
+    @next_shift = Shift.where('shift_time > ?', Time.now).order(shift_time: :asc).first
+    @schedules = Schedule.all
   end
 end
